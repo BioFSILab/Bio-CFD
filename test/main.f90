b9d2b8f866fe5cb61bbb158ef84eb73a93e5a8d2
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-03-24 14:28:22 +0000

    Add FPM testing boilerplate

---
+++ test/main.f90
@@ -0,0 +1,16 @@
+program tester
+  use, intrinsic :: iso_fortran_env, only : error_unit
+  use testdrive, only : run_testsuite
+  use test_demo, only : collect_demo
+  implicit none
+  integer :: stat
+
+  stat = 0  
+  call run_testsuite(collect_demo, error_unit, stat)
+
+  if (stat > 0) then
+    write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+    error stop
+  end if
+
+end program tester
