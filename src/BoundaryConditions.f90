d37dbeda1868416569376e19e950fbccbef259c5
mcbarton <150042563+mcbarton@users.noreply.github.com>
2025-04-09 15:24:47 +0100

    Fix Fortitude linter issues with BoundaryConditions.f90 except C121

--- src/BoundaryConditions.f90
+++ src/BoundaryConditions.f90
@@ -1,17 +1,19 @@
 module biocfd_boundary_conditions
+  use, intrinsic :: iso_fortran_env, only: dp => real64, int64
   use global
   implicit none
+  private
+
+  public :: velocityBC, solidCellBC, solidCellBC_move
 
   contains
 !***********************************************************************
 SUBROUTINE velocityBC
-      USE global
-      INTEGER, PARAMETER :: rk = selected_real_kind(8)
-      INTEGER (KIND = 8):: i, j, k, g
-      REAL (KIND = 8) :: a1, b1, c1, a2, b2, c2, a3, b3, c3, a4, b4, c4, &
+      INTEGER (int64):: i, j, k, g
+      REAL (dp) :: a1, b1, c1, a2, b2, c2, a3, b3, c3, a4, b4, c4, &
                          a5, b5, c5, a6, b6, c6, a7, b7, c7, a8, b8, c8, &
                          x3, uinlet, varx1
-      REAL (KIND = 8) :: a00, qinlet, inletArea, outletArea
+      REAL (dp) :: a00, qinlet, inletArea, outletArea
 
     !uc=1._rk
 
@@ -53,9 +55,19 @@ SUBROUTINE velocityBC
       ! block(g)%vt(block(g)%nx+2,j,k)  = - block(g)%vt(block(g)%nx+1,j,k)
       ! block(g)%wt(block(g)%nx+2,j,k)  = - block(g)%wt(block(g)%nx+1,j,k)
         !Orlanski - vortex shedding Re Convective flow (outlet)
-        block(g)%ut(block(g)%nx+1,j,k)  =  block(g)%u(block(g)%nx+1,j,k)-(deltat/block(g)%deltax(block(g)%nx+1))*uc*(block(g)%u(block(g)%nx+1,j,k)-block(g)%u(block(g)%nx,j,k))
-        block(g)%vt(block(g)%nx+2,j,k)  = -block(g)%vt(block(g)%nx+1,j,k) + block(g)%v(block(g)%nx+2,j,k) + block(g)%v(block(g)%nx+1,j,k) - (2._rk*deltat/block(g)%deltax(block(g)%nx+2))*uc*(block(g)%v(block(g)%nx+2,j,k)-block(g)%v(block(g)%nx+1,j,k))
-        block(g)%wt(block(g)%nx+2,j,k)  = -block(g)%wt(block(g)%nx+1,j,k) + block(g)%w(block(g)%nx+2,j,k) + block(g)%w(block(g)%nx+1,j,k) - (2._rk*deltat/block(g)%deltax(block(g)%nx+2))*uc*(block(g)%w(block(g)%nx+2,j,k)-block(g)%w(block(g)%nx+1,j,k))
+      block(g)%ut(block(g)%nx+1,j,k) = block(g)%u(block(g)%nx+1,j,k)- &
+                                      (deltat/block(g)%deltax(block(g)%nx+1))*&
+                                      uc*(block(g)%u(block(g)%nx+1,j,k)-block(g)%u(block(g)%nx,j,k))
+      block(g)%vt(block(g)%nx+2,j,k) = -block(g)%vt(block(g)%nx+1,j,k)+ &
+                                        block(g)%v(block(g)%nx+2,j,k)+ &
+                                        block(g)%v(block(g)%nx+1,j,k)- &
+                                        (2._dp*deltat/block(g)%deltax(block(g)%nx+2))*&
+                                    uc*(block(g)%v(block(g)%nx+2,j,k)-block(g)%v(block(g)%nx+1,j,k))
+      block(g)%wt(block(g)%nx+2,j,k) = -block(g)%wt(block(g)%nx+1,j,k)+
+                                       block(g)%w(block(g)%nx+2,j,k)+ &
+                                       block(g)%w(block(g)%nx+1,j,k)- &
+                                       (2._dp*deltat/block(g)%deltax(block(g)%nx+2))*&
+                                    uc*(block(g)%w(block(g)%nx+2,j,k)-block(g)%w(block(g)%nx+1,j,k))
       !  endif
         END DO
         END DO
@@ -74,13 +86,13 @@ SUBROUTINE velocityBC
           block(g)%wt(i,1,k) = block(g)%wt(i,2,k)
         !block(g)%ut(i,1,k) =  block(g)%ut(i,2,k)
         !block(g)%wt(i,1,k) =  block(g)%wt(i,2,k)
-         block(g)%vt(i,2,k) =  0._rk
+         block(g)%vt(i,2,k) =  0._dp
 
           block(g)%ut(i,block(g)%ny+2,k) = block(g)%ut(i,block(g)%ny+1,k)                                      !wall no slip - closed channel
           block(g)%wt(i,block(g)%ny+2,k) = block(g)%wt(i,block(g)%ny+1,k)
         !block(g)%ut(i,block(g)%ny+2,k) =  block(g)%ut(i,block(g)%ny+1,k)                                      !symmetric bc - open channel
         !block(g)%wt(i,block(g)%ny+2,k) =  block(g)%wt(i,block(g)%ny+1,k)
-         block(g)%vt(i,block(g)%ny+1,k) =  0._rk
+         block(g)%vt(i,block(g)%ny+1,k) =  0._dp
 
 
 
@@ -96,13 +108,13 @@ SUBROUTINE velocityBC
          block(g)%vt(i,j,1) =  block(g)%vt(i,j,2)
         !block(g)%ut(i,j,1) =  block(g)%ut(i,j,2)
         !block(g)%vt(i,j,1) =  block(g)%vt(i,j,2)
-         block(g)%wt(i,j,1) =  0._rk
+         block(g)%wt(i,j,1) =  0._dp
 
          block(g)%ut(i,j,block(g)%nz+2) =  block(g)%ut(i,j,block(g)%nz+1)                                      !wall no slip - closed channel
          block(g)%vt(i,j,block(g)%nz+2) =  block(g)%vt(i,j,block(g)%nz+1)
         !block(g)%ut(i,j,block(g)%nz+2) =  block(g)%ut(i,j,block(g)%nz+1)                                      !symmetric bc - open channel
         !block(g)%vt(i,j,block(g)%nz+2) =  block(g)%vt(i,j,block(g)%nz+1)
-         block(g)%wt(i,j,block(g)%nz+1) =  0._rk
+         block(g)%wt(i,j,block(g)%nz+1) =  0._dp
 
       END DO
       END DO
@@ -112,9 +124,7 @@ SUBROUTINE velocityBC
 
 !***********************************************************************
       SUBROUTINE solidCellBC
-         USE global
-         INTEGER, PARAMETER :: rk = selected_real_kind(8)
-         INTEGER (KIND = 8):: i, j, k, n, g
+         INTEGER (int64):: i, j, k, n, g
         !!$acc parallel loop gang vector &
         !!$acc present(solidIndexPtr, ut, vt, wt, p) &
         !!$acc private (i, j, k)
@@ -127,10 +137,10 @@ SUBROUTINE velocityBC
             i = block(g)%solidIndexPtr(n,1)
             j = block(g)%solidIndexPtr(n,2)
 	     k = block(g)%solidIndexPtr(n,3)
-            block(g)%ut(i,j,k) = 0._rk
-            block(g)%vt(i,j,k) = 0._rk
-            block(g)%wt(i,j,k) = 0._rk
-            block(g)%p(i,j,k)  = 0._rk
+            block(g)%ut(i,j,k) = 0._dp
+            block(g)%vt(i,j,k) = 0._dp
+            block(g)%wt(i,j,k) = 0._dp
+            block(g)%p(i,j,k)  = 0._dp
          END DO
         !$acc end parallel
          END DO
@@ -141,11 +151,9 @@ SUBROUTINE velocityBC
 !**************************************************************
       !SUBROUTINE solidCellBC_move(b_blk_no)
       SUBROUTINE solidCellBC_move(g)
-         USE global
-         INTEGER, PARAMETER :: rk = selected_real_kind(8)
-         INTEGER (KIND = 8):: i, j, k,n, vbcOption
+         INTEGER (int64):: i, j, k,n, vbcOption
          !INTEGER :: g
-         INTEGER (KIND = 8),INTENT(IN):: g
+         INTEGER (int64),INTENT(IN):: g
          ! INTEGER (KIND = 8), INTENT(IN) :: b_blk_no
         !g=b_blk_no
       !  print*,g,'inside_solid'
@@ -159,13 +167,13 @@ SUBROUTINE velocityBC
             i = block(g)%solidIndexPtr(n,1)
             j = block(g)%solidIndexPtr(n,2)
             k = block(g)%solidIndexPtr(n,3)
-            block(g)%ut(i,j,k) = 0._rk
-            block(g)%vt(i,j,k) = 0._rk
-            block(g)%wt(i,j,k) = 0._rk
-            block(g)%p(i,j,k) = 0._rk
-            block(g)%u(i,j,k) = 0._rk
-            block(g)%v(i,j,k) = 0._rk
-            block(g)%w(i,j,k) = 0._rk
+            block(g)%ut(i,j,k) = 0._dp
+            block(g)%vt(i,j,k) = 0._dp
+            block(g)%wt(i,j,k) = 0._dp
+            block(g)%p(i,j,k) = 0._dp
+            block(g)%u(i,j,k) = 0._dp
+            block(g)%v(i,j,k) = 0._dp
+            block(g)%w(i,j,k) = 0._dp
          END DO
          !$acc end parallel
         endif
