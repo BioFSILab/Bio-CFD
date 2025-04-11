b03776ad792961a3baef28cec35ff95836a18269
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-04-11 11:49:23 +0100

    Update src/CoarseInterp.f90
    Co-authored-by: Tom Neep <t.j.neep@bham.ac.uk>

--- src/CoarseInterp.f90
+++ src/CoarseInterp.f90
@@ -444,7 +444,6 @@ subroutine coarseUpdate
         enddo
         ENDDO
         end subroutine coarseUpdate_newv
-        
         subroutine coarseUpdate_pc
         REAL (dp) :: bl_intp_valx,bl_intp_valy,bl_intp_x1,bl_intp_x2,bl_intp_y1,&
                bl_intp_y2,bl_intp_f1,bl_intp_f2,bl_intp_f3,bl_intp_f4
