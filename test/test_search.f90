a157a53ad2f955fb0a19b59b7852b94074a4bfea
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-04-14 15:41:09 +0100

    Add test for shiftSurfaceNodesInitial

--- test/test_search.f90
+++ test/test_search.f90
@@ -13,7 +13,10 @@ contains
     !> Collection of tests
     type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-    testsuite = [new_unittest("find_dist_node", test_find_dist_node)]
+    testsuite = [&
+      new_unittest("find_dist_node", test_find_dist_node), &
+      new_unittest("shift_surface_nodes_initial", test_shift_surface_nodes_initial_simple) &
+    ]
   end subroutine collect_search
 
 
@@ -26,10 +29,11 @@ contains
     integer(int64) :: expected
 
     ! Setup the required block variables
+    if (allocated(block)) deallocate(block)
     blk_start = 1
     nblocks = 1
     allocate(block(nblocks))
-    block(1)%ibnodes = 6
+    block(1)%ibNodes = 6
     block(1)%ibNodeId = [51, 51, 51, 51, 51, 51]
     block(1)%xnode = [0, -7, -4, 1, -10, 8]
     block(1)%ynode = [0, -7, -4, 1, -10, 8]
@@ -40,4 +44,44 @@ contains
     call check(error, block(1)%mk, expected)
     if (allocated(error)) return
   end subroutine test_find_dist_node
+
+  !> Test the shiftSurfaceNodesInitial subroutine but only for the
+  !> branch where the node ID is not equal to 51 or 52. This is the
+  !> case where the expected output just becomes e.g. xnode + xshift
+  subroutine test_shift_surface_nodes_initial_simple(error)
+    use global, only : block, blk_start, nblocks
+    use biocfd_search, only : shiftSurfaceNodesInitial
+    type(error_type), allocatable, intent(out) :: error
+    integer :: i
+
+    ! Setup the required block variables
+    if (allocated(block)) deallocate(block)
+    blk_start = 1
+    nblocks = 1
+    allocate(block(nblocks))
+    block(1)%ibNodes = 1
+    ! As mentioned above, when node ID is 50, the code branch is
+    ! relatively simple
+    block(1)%ibNodeId = [50]
+    block(1)%xnode = [1]
+    block(1)%ynode = [2]
+    block(1)%znode = [3]
+
+    ! Set block shift up
+    block(1)%xshift = 1
+    block(1)%yshift = 2
+    block(1)%zshift = 3
+
+    call shiftSurfaceNodesInitial
+
+    call check(error, all(block(1)%xnode1 == block(1)%xnode + block(1)%xshift))
+    if (allocated(error)) return
+
+    call check(error, all(block(1)%ynode1 == block(1)%ynode + block(1)%yshift))
+    if (allocated(error)) return
+
+    call check(error, all(block(1)%znode1 == block(1)%znode + block(1)%zshift))
+    if (allocated(error)) return
+
+  end subroutine test_shift_surface_nodes_initial_simple
 end module test_search
