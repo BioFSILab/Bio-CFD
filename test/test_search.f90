c2fa34131f0225f5b9e9ea42dbcb7ee936f1af5c
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-04-14 13:22:02 +0100

    Change demo test to test search module

--- test/test_search.f90
+++ test/test_search.f90
@@ -1,25 +1,25 @@
-module test_demo
+module test_search
   use testdrive, only : error_type, unittest_type, new_unittest, check
   implicit none
   private
 
-  public :: collect_demo
+  public :: collect_search
 
 contains
 
   !> Collect all exported unit tests
-  subroutine collect_demo(testsuite)
+  subroutine collect_search(testsuite)
     !> Collection of tests
     type(unittest_type), allocatable, intent(out) :: testsuite(:)
 
-    testsuite = [new_unittest("substitute", test_substitute)]
-  end subroutine collect_demo
+    testsuite = [new_unittest("find_dist_node", test_find_dist_node)]
+  end subroutine collect_search
 
-  !> Check substitution of a single line
-  subroutine test_substitute(error)
+
+  subroutine test_find_dist_node(error)
     !> Error handling
     type(error_type), allocatable, intent(out) :: error
 
     call check(error, "This is a valid example", "This is a valid example")
-  end subroutine test_substitute
-end module test_demo
+  end subroutine test_find_dist_node
+end module test_search
