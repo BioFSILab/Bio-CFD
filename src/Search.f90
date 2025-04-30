52651e8cfbeb6076f46de87ab407f9336f7e7cb7
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-04-30 15:31:07 +0100

    Remove scar tissue.
    Timing now around 9m30s on my M1 Pro Macbook Pro

--- src/Search.f90
+++ src/Search.f90
@@ -303,7 +303,7 @@ module biocfd_search
 
      SUBROUTINE tagging_th
 
-        INTEGER(int64) :: g, n, m, i, j, k,  nel2Cen, nel2Pnt, sumNodeId
+        INTEGER(int64) :: g, n, i, j, k,  nel2Cen, nel2Pnt, sumNodeId
         REAL(dp)      :: n2dotn
         !> This temporary array holds distance calculations
         real(dp), allocatable, dimension(:) :: temp_var
@@ -324,29 +324,14 @@ module biocfd_search
         DO k = block(g)%k_startSearch, block(g)%k_endSearch
         DO j = block(g)%j_startSearch, block(g)%j_endSearch
         DO i = block(g)%i_startSearch, block(g)%i_endSearch
-            ! minDis  = 1e14_dp
-            ! minDis1 = 1e14_dp
 
-            temp_var = dsqrt( (block(g)%yp(j)-block(g)%ycent)**2 + (block(g)%xp(i)-block(g)%xcent)**2  + (block(g)%zp(k)-block(g)%zcent)**2)
+            ! There is no need to take the sqrt of these distances as we just want the minimum
+            temp_var = (block(g)%yp(j)-block(g)%ycent)**2 + (block(g)%xp(i)-block(g)%xcent)**2 &
+                     + (block(g)%zp(k)-block(g)%zcent)**2
             nel2cen = minloc(temp_var, dim=1)
-            temp_var = dsqrt( (block(g)%y1(j)-block(g)%ycent)**2 + (block(g)%x1(i)-block(g)%xcent)**2  + (block(g)%z1(k)-block(g)%zcent)**2)
+            temp_var = (block(g)%y1(j)-block(g)%ycent)**2 + (block(g)%x1(i)-block(g)%xcent)**2 &
+                     + (block(g)%z1(k)-block(g)%zcent)**2
             nel2pnt = minloc(temp_var, dim=1)
-            ! !$acc loop seq
-            ! DO m = 1, block(g)%ibElems
-            ! cent_x = block(g)%xcent(m)
-            ! cent_y = block(g)%ycent(m)
-            ! cent_z = block(g)%zcent(m)
-            !    dis_cen  = dsqrt( (block(g)%yp(j)-cent_y)**2 + (block(g)%xp(i)-cent_x)**2  + (block(g)%zp(k)-cent_z)**2)
-            !    dis_pnt  = dsqrt( (block(g)%y1(j)-cent_y)**2 + (block(g)%x1(i)-cent_x)**2  + (block(g)%z1(k)-cent_z)**2)
-            !    IF (dis_cen<minDis) THEN
-            !       minDis    = dis_cen
-            !       nel2Cen   = m
-            !    ENDIF
-            !    IF (dis_pnt<minDis1) THEN
-            !       minDis1   = dis_pnt
-            !       nel2Pnt   = m
-            !    ENDIF
-            ! ENDDO
 
             IF((block(g)%x1(i)<=block(g)%xcent(nel2Cen) .AND. &
                 block(g)%x1(i+1)>=block(g)%xcent(nel2Cen)).AND. &
