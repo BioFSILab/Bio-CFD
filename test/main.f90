c8fb44c13783a0515685c682f42fa59219e66fa7
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-05-20 14:39:49 +0100

    Add tests for interpolation module

--- test/main.f90
+++ test/main.f90
@@ -1,12 +1,24 @@
 program tester
   use, intrinsic :: iso_fortran_env, only : error_unit
-  use testdrive, only : run_testsuite
+  use testdrive, only : run_testsuite, new_testsuite, testsuite_type
   use test_search, only : collect_search
+  use test_interpolation, only: collect_interpolation
   implicit none
-  integer :: stat
+  integer :: stat, is
+  type(testsuite_type), allocatable :: testsuites(:)
+  character(len=*), parameter :: fmt = '("#", *(1x, a))'
 
   stat = 0
-  call run_testsuite(collect_search, error_unit, stat)
+
+  testsuites = [ &
+    new_testsuite("search", collect_search), &
+    new_testsuite("interpolation", collect_interpolation) &
+    ]
+
+  do is = 1, size(testsuites)
+    write(error_unit, fmt) "Testing:", testsuites(is)%name
+    call run_testsuite(testsuites(is)%collect, error_unit, stat)
+  end do
 
   if (stat > 0) then
     write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
