299a671ea5f4df8567894f6928c0d0f48a2ab2e8
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-07-08 15:16:22 +0100

    Reorder imports

--- src/PcorVcor.F90
+++ src/PcorVcor.F90
@@ -1,11 +1,11 @@
 module biocfd_pcor_vcor
   use, intrinsic :: iso_fortran_env, only: dp => real64, int64
   ! allow(use-all) - TODO: Aim to fix this in the future
+  use global
   use omp_lib
 #ifdef _OPENACC
   use openacc
 #endif
-  use global
   use biocfd_fine_interp_bound, only : fineUpdate_newv_bd, fineUpdate_bd, fineUpdate_pc_bd
   use biocfd_coarse_update, only : coarseUpdate_newv, coarseUpdate_pc, coarseUpdate
   use biocfd_boundary_conditions, only : velocityBC
