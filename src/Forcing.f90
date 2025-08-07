1a4045554e0e2da4fb06c5b03ee9b2f6b0ccd734
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-08-07 12:45:08 +0100

    Add ac_x to use global in Forcing.f90 (needed for openacc build)

--- src/Forcing.f90
+++ src/Forcing.f90
@@ -1,6 +1,6 @@
 module biocfd_forcing
   use, intrinsic :: iso_fortran_env, only: dp => real64
-  use global, only : block, blk_start, nblocks, ac_y, ac_z, at_y, at_z
+  use global, only : block, blk_start, nblocks, ac_y, ac_z, at_y, at_z, ac_x
   use biocfd_interpolation, only: linear_interpolation, bilinear_interpolation
   implicit none
 
