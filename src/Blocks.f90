25ad26d58f6f0b75d2d2c6c1e71d8726152acd79
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-08-11 10:28:27 +0100

    Remove unused nnz, nu, nit1 variables

--- src/Blocks.f90
+++ src/Blocks.f90
@@ -38,7 +38,7 @@ MODULE biocfd_blocks
         INTEGER(int64) :: ibCellCount, solidCellCount, fluidCellCount, redCellCount, &
                           blackCellCount, TSCellCount
 
-        INTEGER (int32)   :: nnz,nu, nit, nit1
+        INTEGER (int32)   :: nit
         INTEGER (int32), ALLOCATABLE, DIMENSION (:) :: row_ptr,col
         REAL (dp), ALLOCATABLE, DIMENSION (:) :: sol
         INTEGER (int32) :: crs_data(4)
