6e891a67e5a10afc22492079b8ce69ed3c218797
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-08-13 15:38:33 +0100

    Fix test for lfortran.
    Really we could delete this test as I'm not sure it
    is doing much good at the moment but will keep it for now.

--- test/test_search.f90
+++ test/test_search.f90
@@ -24,12 +24,26 @@ contains
     type(error_type), allocatable, intent(out) :: error
 
     integer(int64) :: expected
+    integer :: ibnodes
 
     ! Setup the required block variables
     blk_start = 1
     nblocks = 1
+    ibnodes = 6
     allocate(block(nblocks))
-    block(1)%ibnodes = 6
+    block(1)%ibnodes = ibnodes
+
+    ! Although not required by fortran>=2003 Lfortran requires arrays
+    ! to be allocated before they are assigned to see
+    ! https://github.com/lfortran/lfortran/issues/2946
+    allocate(&
+         block(1)%ibNodeId(ibnodes), &
+         block(1)%xnode(ibnodes), &
+         block(1)%ynode(ibnodes), &
+         block(1)%znode(ibnodes) &
+    )
+    
+      
     block(1)%ibNodeId = [51, 51, 51, 51, 51, 51]
     block(1)%xnode = [0, -7, -4, 1, -10, 8]
     block(1)%ynode = [0, -7, -4, 1, -10, 8]
