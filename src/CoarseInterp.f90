999e48db85733b1a0ed8b610958f32fd846ec5ce
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-08-19 13:37:22 +0100

    CoarseInterp MPI

--- src/CoarseInterp.f90
+++ src/CoarseInterp.f90
@@ -2,7 +2,9 @@ module biocfd_coarse_update
   use, intrinsic :: iso_fortran_env, only: dp => real64
   use global, only : block, intfr, intflines
   use biocfd_interpolation, only: bilinear_interpolation, linear_interpolation
-
+#ifdef BIOCFD_MPI
+  use mpi_f08
+#endif
   implicit none
   private
 
@@ -139,9 +141,46 @@ subroutine coarseUpdate
         INTEGER :: st_idy, en_idy
         INTEGER :: st_idz, en_idz
 
+#ifdef BIOCFD_MPI
+        integer :: rank, num_proc, ierror, token
+        call MPI_Comm_size(MPI_COMM_WORLD, num_proc, ierror)
+        call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
+
+        ! TN: I *think* that this interpolation needs to essentially
+        ! happen serially across ranks. The idea is that rank 0 will
+        ! run first, then update block(1) on rank 1 and tell rank 1 it
+        ! can start. Rank 1 would then update the block on rank 2 and
+        ! tell rank 2 it can start. This process will repeat up to
+        ! rank n. Once rank n finishes we broadcast the block(1)
+        ! arrays to all ranks so that everything is up to date.
+        !
+        ! We don't wait for anything if we are on rank 0 and just get
+        ! started
+        if (rank /= 0) then
+          ! This essentially acts as a barrier in that rank n wont
+          ! start until rank n-1 has finished. Assumes that a_blk number is always 1
+          call MPI_Recv(block(1)%ut, size(block(1)%ut), MPI_DOUBLE_PRECISION, rank-1, 0, &
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+          call MPI_Recv(block(1)%vt, size(block(1)%vt), MPI_DOUBLE_PRECISION, rank-1, 0, &
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+          call MPI_Recv(block(1)%wt, size(block(1)%wt), MPI_DOUBLE_PRECISION, rank-1, 0, &
+                        MPI_COMM_WORLD, MPI_STATUS_IGNORE, ierror)
+        end if
+
+#endif
+
         DO g=1,intflines
         a_blk_no=intfr(g)%a_blk
         b_blk_no=intfr(g)%b_blk
+
+#ifdef BIOCFD_MPI
+        if (.not. allocated(block(b_blk_no)%ut)) then
+            ! If this array isn't allocated we aren't on the right
+            ! rank to deal with this so keep going until we find
+            ! one that is on this rank
+            cycle
+        end if
+#endif
         st_idx=block(b_blk_no)%cintp
         en_idx=intfr(g)%counterxu-block(b_blk_no)%cintp
         st_idy=block(b_blk_no)%cintp
@@ -259,6 +298,36 @@ subroutine coarseUpdate
         enddo
         enddo
         ENDDO
+
+#ifdef BIOCFD_MPI
+        ! We have no finished processing the ut, vt, and wt arrays on
+        ! rank n - send these arrays to rank n+1 to continue, unless
+        ! we are the last rank, in which case we need will broadcast
+        ! the updated array to all ranks (this is maybe something that
+        ! can be optimised as we might not need everything to be up to
+        ! date here)
+        if (rank /= (num_proc-1)) then
+          call MPI_Send(block(1)%ut, size(block(1)%ut), MPI_DOUBLE_PRECISION, rank+1, 0, &
+                        MPI_COMM_WORLD, ierror)
+          call MPI_Send(block(1)%vt, size(block(1)%vt), MPI_DOUBLE_PRECISION, rank+1, 0, &
+                        MPI_COMM_WORLD, ierror)
+          call MPI_Send(block(1)%wt, size(block(1)%wt), MPI_DOUBLE_PRECISION, rank+1, 0, &
+                        MPI_COMM_WORLD, ierror)
+        end if
+
+        ! This will block everything until the last rank is done
+        call MPI_Bcast(block(1)%ut, size(block(1)%ut), MPI_DOUBLE_PRECISION, num_proc-1, &
+                       MPI_COMM_WORLD)
+        call MPI_Bcast(block(1)%vt, size(block(1)%vt), MPI_DOUBLE_PRECISION, num_proc-1, &
+                       MPI_COMM_WORLD)
+        call MPI_Bcast(block(1)%wt, size(block(1)%wt), MPI_DOUBLE_PRECISION, num_proc-1, &
+                       MPI_COMM_WORLD)
+        ! Now block(1) ut, vt, and wt, should be the same on all ranks
+        print *, "Rank = ", rank, "block(1)%ut(1, 1, 1) = ", block(1)%ut(1, 1, 1)
+        print *, "Rank = ", rank, "block(1)%vt(1, 1, 1) = ", block(1)%vt(1, 1, 1)
+        print *, "Rank = ", rank, "block(1)%wt(1, 1, 1) = ", block(1)%wt(1, 1, 1)
+#endif
+
         end subroutine coarseUpdate_newv
         subroutine coarseUpdate_pc
 
