04349845cfa6daa71df69e6e5e8e6c77e05ebc32
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-08-22 16:58:44 +0100

    Fix bug in coarse interpolation with refactor

--- src/CoarseInterp.f90
+++ src/CoarseInterp.f90
@@ -126,13 +126,6 @@ subroutine coarseUpdate
         end subroutine coarseUpdate
 
         subroutine coarseUpdate_newv
-        REAL (dp) :: bl_intp_valx,bl_intp_valy,bl_intp_x1,bl_intp_x2,bl_intp_y1,bl_intp_y2,&
-               bl_intp_f1,bl_intp_f2,bl_intp_f3,bl_intp_f4
-        REAL (dp) :: bl_intp_valz,bl_intp_z1,bl_intp_z2
-        REAL (dp) :: bl_intp_deno, bl_intp_num, bl_intp_xtx, bl_intp_xxo, bl_intp_yty,&
-               bl_intp_yyo, bl_intp_first_term, bl_intp_second_term
-        REAL (dp) :: bl_interp_ans1, bl_interp_ans2
-        REAL (dp) :: bl_interp_ans
 
         INTEGER :: i,j,k,g, tar_x, tar_y, tar_z, loc_x, loc_y, loc_z, a_blk_no,b_blk_no
         INTEGER :: st_idx, en_idx
@@ -209,51 +202,12 @@ subroutine coarseUpdate
                 loc_x=(intfr(g)%wx_interface_det(2,i)+intfr(g)%wx_interface_det(3,i)-1)/2
                 loc_y=(intfr(g)%wy_interface_det(2,j)+intfr(g)%wy_interface_det(3,j)-1)/2
                 loc_z=(intfr(g)%wz_interface_det(3,k))
-                bl_intp_valx=block(a_blk_no)%xw(tar_x)
-                bl_intp_valy=block(a_blk_no)%yw(tar_y)
-                bl_intp_valz=block(a_blk_no)%zw(tar_z)
-                bl_intp_x1=block(b_blk_no)%xw(loc_x)
-                bl_intp_x2=block(b_blk_no)%xw(loc_x+1)
-                bl_intp_y1=block(b_blk_no)%yw(loc_y)
-                bl_intp_y2=block(b_blk_no)%yw(loc_y+1)
-                bl_intp_z1=block(b_blk_no)%zw(loc_z-1)
-                bl_intp_z2=block(b_blk_no)%zw(loc_z)
-                bl_intp_f1=block(b_blk_no)%wt(loc_x,loc_y,loc_z-2)
-                bl_intp_f2=block(b_blk_no)%wt(loc_x+1,loc_y,loc_z-2)
-                bl_intp_f3=block(b_blk_no)%wt(loc_x+1,loc_y+1,loc_z-2)
-                bl_intp_f4=block(b_blk_no)%wt(loc_x,loc_y+1,loc_z-2)
 
-                bl_intp_deno= (bl_intp_x2-bl_intp_x1) * (bl_intp_y2-bl_intp_y1)
-                bl_intp_xtx= (bl_intp_x2 -bl_intp_valx)
-                bl_intp_xxo=(bl_intp_valx-bl_intp_x1)
-                bl_intp_yty=(bl_intp_y2-bl_intp_valy)
-                bl_intp_yyo=(bl_intp_valy-bl_intp_y1)
-                bl_intp_first_term=(bl_intp_f1*bl_intp_xtx + bl_intp_f2*bl_intp_xxo)*bl_intp_yty
-                bl_intp_second_term=(bl_intp_f4*bl_intp_xtx + bl_intp_f3*bl_intp_xxo)*bl_intp_yyo
-                bl_intp_num= bl_intp_first_term + bl_intp_second_term
-
-                bl_interp_ans1=(bl_intp_num/bl_intp_deno)
-                bl_intp_f1=block(b_blk_no)%w(loc_x,loc_y,loc_z-1)
-                bl_intp_f2=block(b_blk_no)%w(loc_x+1,loc_y,loc_z-1)
-                bl_intp_f3=block(b_blk_no)%w(loc_x+1,loc_y+1,loc_z-1)
-                bl_intp_f4=block(b_blk_no)%w(loc_x,loc_y+1,loc_z-1)
-                bl_intp_first_term=(bl_intp_f1*bl_intp_xtx + bl_intp_f2*bl_intp_xxo)*bl_intp_yty
-                bl_intp_second_term=(bl_intp_f4*bl_intp_xtx + bl_intp_f3*bl_intp_xxo)*bl_intp_yyo
-                bl_intp_num= bl_intp_first_term + bl_intp_second_term
-
-                bl_interp_ans2=(bl_intp_num/bl_intp_deno)
-                bl_interp_ans= bl_interp_ans1 + ((bl_intp_valz-bl_intp_z1)*&
-                     ((bl_interp_ans2 - bl_interp_ans1)/(bl_intp_z2 - bl_intp_z1)))
-                block(a_blk_no)%wt(tar_x,tar_y,tar_z-1)=bl_interp_ans
-
-                ! The following block can replace the above once we have an answer on GH issue #116
-                ! block(a_blk_no)%wt(tar_x, tar_y, tar_z-1) = trilinear_interpolation( &
-                !      block(a_blk_no)%xw(tar_x), block(a_blk_no)%yw(tar_y), block(a_blk_no)%zw(tar_z), &
-                !      loc_x, loc_y, loc_z, &
-                !      block(b_blk_no)%xw, block(b_blk_no)%yw, block(b_blk_no)%zw, &
-                !      3, block(b_blk_no)%wt)
-
-
+                block(a_blk_no)%wt(tar_x, tar_y, tar_z-1) = trilinear_interpolation( &
+                     block(a_blk_no)%xw(tar_x), block(a_blk_no)%yw(tar_y), block(a_blk_no)%zw(tar_z), &
+                     loc_x, loc_y, loc_z, &
+                     block(b_blk_no)%xw, block(b_blk_no)%yw, block(b_blk_no)%zw, &
+                     3, block(b_blk_no)%wt)
 
         enddo
         enddo
