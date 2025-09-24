05ed64630cb6c7008d6ba62b4003b62b7d30c509
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-09-24 14:36:06 +0100

    Remove unused nIterPcor variable

--- src/modGlobal.f90
+++ src/modGlobal.f90
@@ -8,7 +8,7 @@ MODULE global
        CHARACTER (LEN = 3)   :: char_f
        INTEGER               :: istart
        INTEGER (int64)   :: itamax, pcItaMax,amgxita,      &
-                                ita, nIterPcor, ita1,ita2,    &
+                                ita, ita1,ita2,    &
                                 inor,blk_start, coarse_flcnt_check
        REAL (dp)        ::      dt_order,  &
                                 omega,omega1,omega2,omega3,omega4,  &
