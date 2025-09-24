abb1a5c7368cd8d3c10e302b34464f83a9e20fcf
mcbarton <150042563+mcbarton@users.noreply.github.com>
2025-09-24 15:00:59 +0100

    Remove unused variable al

--- src/modGlobal.f90
+++ src/modGlobal.f90
@@ -18,7 +18,7 @@ MODULE global
                                 epsi, re, rev, &
                                 alpha, &
                                 xfact,deltat,totime, totalTime, &
-                                pi, al, uc
+                                pi, uc
 
        REAL (dp)       :: alpha_m, theta_m, alpha_m1, theta_m1, mu_f, rho_f, l_c, u_tip, disp
 
