3db3617c594ea8d951df45c105e81188a67c612a
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-01 11:34:25 +0100

    Remove block, blk_start, nblocks from use global BoundaryConditions.f90

--- src/BoundaryConditions.f90
+++ src/BoundaryConditions.f90
@@ -1,6 +1,6 @@
 module biocfd_boundary_conditions
   use, intrinsic :: iso_fortran_env, only: dp => real64, int64
-  use global, only : block, blk_start, nblocks, deltat, uc
+  use global, only : deltat, uc
   use biocfd_blocks,only : Blocks
   implicit none
   private
