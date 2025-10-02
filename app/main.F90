03630e29152957bd1dbe400d6969b05c9b020f68
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-10-02 15:10:29 +0100

    Use explicit blk argument in LastConditions

--- app/main.F90
+++ app/main.F90
@@ -3,7 +3,7 @@
         use, intrinsic :: iso_fortran_env, only: int64, dp => real64
         USE global, only: block, blk_start, coarse_flcnt_check, deltat, &
              ita, ita1, nblocks, totaltime, totime,alpha_m, &
-             aoa,aoa1,aoa2,phase_angle,pi,theta_m, uc
+             aoa,aoa1,aoa2,phase_angle,pi,theta_m, uc, re
         use biocfd_search, only: findDistnode, shiftSurfaceNodesInitial, computeSurfaceNorm, &
              tagging_th, tagging_th_move, block_move_check, cellcount_solid, &
              cellcount_solid_coarse, cellcount_solid_coarse_mv, change_block_coords, &
@@ -83,7 +83,14 @@
            end do
            print*, 'initial'
         end if
-        IF (iStart==1) CALL lastConditions
+        IF (iStart==1) then
+           WRITE(*,*) 'Enter lastconditions'
+           ita = 0
+           ita1 = 0
+           do g=1, size(block)
+              call lastConditions(block(g), g, re)
+           end do
+        end if
         do g=blk_start, size(block)
            CALL computeNormDistance(block(g))
         end do
