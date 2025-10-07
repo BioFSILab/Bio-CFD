4fd399fa700a600d6454ff4716e6693085a966d7
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-07 12:40:08 +0100

    Remove rev from use global in ReadInput.f90

--- src/ReadInput.f90
+++ src/ReadInput.f90
@@ -1,7 +1,7 @@
 module biocfd_read_input
   use, intrinsic :: iso_fortran_env, only: dp => real64, int64
   use global, only : block, uc, u_tip, u0, totime, theta_m1, theta_m, &
-       rho_f, rev, re, piv_pt, pi, phase_angle, pcitamax, omega4, &
+       rho_f, re, piv_pt, pi, phase_angle, pcitamax, omega4, &
        omega3, omega2, omega1, nblocks, mu_f, l_c, itamax, ita1, ita, istart, &
        intflines, inor, freq, epsi, dxmin, disp, deltat, char_f, &
        blk_start, aoa, alpha_m1, alpha_m, alpha, intfr, Interfaces
