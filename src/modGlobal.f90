6c95bd9e4782da2b90750516124945826d9cc454
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-10-10 11:03:29 +0100

    Make pi a parameter

--- src/modGlobal.f90
+++ src/modGlobal.f90
@@ -14,7 +14,8 @@ MODULE global
                                 epsi, re, &
                                 alpha, &
                                 deltat, totime, totalTime, &
-                                pi, uc
+                                uc
+       real(dp), parameter :: pi = 4._dp * atan(1._dp)
 
        REAL (dp)       :: alpha_m, theta_m, alpha_m1, theta_m1, mu_f, rho_f, l_c, u_tip, disp
 
