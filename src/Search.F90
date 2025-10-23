23188c9f40e2178f43baf423150850523aeb196e
mcbarton <matthew.c.barton@hotmail.co.uk>
2025-10-23 15:54:37 +0100

    Define needed integers i,j and k in tagging_th

--- src/Search.F90
+++ src/Search.F90
@@ -384,6 +384,7 @@ SUBROUTINE tagging_th_core(blk)
        type(Blocks), intent(inout) :: blk
        INTEGER(int64), intent(in)  :: blk_no
        CHARACTER(LEN=120) :: filename1
+       INTEGER(int64) :: i, j, k 
 
         call tagging_th_core(blk)
                WRITE(filename1,1) blk_no
