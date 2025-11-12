eee371e45117d842a167af766644eeac2cfc2bb8
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-11-12 11:30:32 +0000

    Make sure we don't initialise until after readInput

--- app/main.F90
+++ app/main.F90
@@ -36,9 +36,10 @@
         INTEGER (int64)   :: itamax, pcItaMax
         real(dp) :: aoa,aoa1,aoa2,phase_angle,piv_pt
         integer :: start, finish, step, rank
-        call biocfd_init(size(block), start, finish, step, rank)
 
         CALL readInput(surGeoPoints,char_f,istart,itamax,pcItaMax,aoa,phase_angle,piv_pt)
+        ! Need to init after we know the size of block
+        call biocfd_init(size(block), start, finish, step, rank)
         CALL readBlockInterface
         do g=blk_start, size(block)
           CALL readSurfaceMeshGmsh(block(g),surGeoPoints)
