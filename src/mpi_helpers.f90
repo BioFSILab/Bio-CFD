0a55728dd717b7db175d3ea666b2e8498939138d
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-12-02 12:32:51 +0000

    Add subroutine for splitting work more effectively across GPUs

--- src/mpi_helpers.f90
+++ src/mpi_helpers.f90
@@ -7,6 +7,9 @@ module biocfd_mpi_helpers
   use mpi_f08, only: MPI_Abort, MPI_Comm_rank, MPI_Comm_size, MPI_COMM_WORLD, MPI_Finalize, &
                      MPI_Init_Thread, MPI_THREAD_SERIALIZED
 #endif
+#ifdef _OPENACC
+  use openacc, only: acc_device_default, acc_get_num_devices
+#endif
 
   implicit none
 
@@ -90,5 +93,66 @@ subroutine get_block_iteration_params(nblocks, start, finish, step, rank)
 #endif
 
 end subroutine get_block_iteration_params
+
+!> Work out how work will be shared across MPI nodes when we have
+!> multiple GPUs! The idea here is that we will work out how many GPUs
+!> our rank has and how many GPUs there are overall. We can then split
+!> the blocks proportionally across nodes. Each rank must have at
+!> least one GPU for this to work.
+subroutine get_block_iteration_params_gpu(nblocks, start, finish, step, rank)
+   !> The total number of blocks we are simulating
+    integer, intent(in) :: nblocks
+    !> The start, finish (both end and stop are keywords) and step size we will use to loop over blocks
+    integer, intent(out) :: start, finish, step
+    !> The MPI rank that we are running on (0 in the case we aren't using MPI)
+    integer, intent(out) :: rank
+#if defined(BIOCFD_MPI) && defined(_OPENACC)
+    ! An error value to check when using MPI
+    integer :: ierror
+
+    integer :: world_size
+    integer, allocatable :: rank_gpus(:), rank_blocks(:)
+    integer :: total_gpus
+    !> This is blocks per GPU computed as nblocks / total_gpus (real
+    !> as in a floating point number)
+    real :: real_blocks_per_gpu
+    ! Looping variable
+    integer:: i
+
+    call MPI_Comm_size(MPI_COMM_WORLD, world_size, ierror)
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
+
+    ! Get an array of GPUs and fill accordingly
+    allocate(rank_gpus(world_size))
+    rank_gpus(rank + 1) = acc_get_num_devices(acc_device_default)
+
+    ! Gather all GPUs together such that every rank has an array of
+    ! GPU numbers
+    call MPI_Allgather(MPI_IN_PLACE, 0, MPI_INTEGER, rank_gpus, 1, MPI_INTEGER, MPI_COMM_WORLD, & 
+                       ierror)
+    total_gpus = sum(rank_gpus)
+
+    ! This is how many blocks we need to have per GPU
+    real_blocks_per_gpu = real(nblocks) / total_gpus
+
+    ! This is the number of blocks we are going to place on each rank
+    allocate(rank_blocks(world_size))
+    ! This is the rounded number of blocks on each rank
+    rank_blocks = nint(rank_gpus * real_blocks_per_gpu)
+
+    start = sum(rank_blocks(1:rank))
+    finsh = sum(rank_blocks(1:rank+1))
+    step = 1
+    print *, "Testing - start = ", start, "finish = ", finish
+#else
+    ! If we aren't using MPI, it is straight forward. Our loop goes
+    ! over every block from 1 in steps of 1. Rank is set to 0.
+    start = 1
+    finish = nblocks
+    step = 1
+    rank = 0
+#endif
+
+end subroutine get_block_iteration_params_gpu
 
 end module biocfd_mpi_helpers
