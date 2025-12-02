ce0e4715e6a99ebc5b23295989da2139b593aba5
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-12-02 12:55:12 +0000

    Ensure we start at block 1

--- src/mpi_helpers.f90
+++ src/mpi_helpers.f90
@@ -140,7 +140,7 @@ subroutine get_block_iteration_params_gpu(nblocks, start, finish, step, rank)
     ! This is the rounded number of blocks on each rank
     rank_blocks = nint(rank_gpus * real_blocks_per_gpu)
 
-    start = sum(rank_blocks(1:rank))
+    start = sum(rank_blocks(1:rank)) + 1
     finish = sum(rank_blocks(1:rank+1))
     step = 1
     print *, "Testing - start = ", start, "finish = ", finish
