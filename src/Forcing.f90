a266d0088eaa82cdd36a0f91cf628cb8d026f48f
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2026-01-21 15:53:33 +0000

    Fix bug in pressureForcingGhost

--- src/Forcing.f90
+++ src/Forcing.f90
@@ -409,7 +409,7 @@ SUBROUTINE pressureForcingGhost(blk)
         END IF
             dpdn = ((ac_z + at_z)* blk%cosGamma(blk%nelp(blk%index_ts(n))) &
                   + (ac_y + at_y)* blk%cosBeta(blk%nelp(blk%index_ts(n)))) &
-                   + (blk%yddot*(blk%cosBeta(blk%index_ts(n))))
+                   + (blk%yddot*(blk%cosBeta(blk%nelp(blk%index_ts(n)))))
 
          sur2nodeDis = -blk%pNormDis(blk%index_ts(n))
 
